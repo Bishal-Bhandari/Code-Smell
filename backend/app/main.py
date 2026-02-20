d17c84ab2e6bc11079c3e684e448d305eba8e6ce
bishal <bishalbhaandari62@gmail.com>
2026-02-20 09:43:48 +0100

    new endpoint added

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.schemas import CodeRequest
 from app.analyzer import analyze_code
 from app.llm_service import review_with_llm
+from app.github_service import get_pr_files
+from app.schemas import PRRequest
 
 app = FastAPI()
 
@@ -18,3 +20,31 @@ def analyze(request: CodeRequest):
         "static_analysis": static_result,
         "llm_review": llm_result
     }
+
+@app.post("/analyze-pr")
+def analyze_pr(request: PRRequest):
+
+    files = get_pr_files(
+        request.owner,
+        request.repo,
+        request.pr_number
+    )
+
+    results = []
+
+    for file in files:
+        static_result = analyze_code(file["code"])
+        llm_result = review_with_llm(file["code"])
+
+        results.append({
+            "filename": file["filename"],
+            "static_analysis": static_result,
+            "llm_review": llm_result
+        })
+
+    return {
+        "total_files": len(results),
+        "results": results
+    }
+
+
