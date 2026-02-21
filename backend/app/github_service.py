9f2db2edfce7c02983c48334cee58af81b031d07
bishal <bishalbhaandari62@gmail.com>
2026-02-21 21:02:21 +0100

    PR coment bot function added

--- backend/app/github_service.py
+++ backend/app/github_service.py
@@ -27,3 +27,12 @@ def get_pr_files(owner: str, repo: str, pr_number: int):
             })
 
     return files_data
+
+def post_pr_comment(owner: str, repo: str, pr_number: int, comment: str):
+    token = os.getenv("GITHUB_TOKEN")
+    g = Github(token)
+
+    repository = g.get_repo(f"{owner}/{repo}")
+    pull_request = repository.get_pull(pr_number)
+
+    pull_request.create_issue_comment(comment)
