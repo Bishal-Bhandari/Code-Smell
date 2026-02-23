d1186fb19b611188a08e2071d14b9017e2c399ed
bishal <bishalbhaandari62@gmail.com>
2026-02-23 11:39:56 +0100

    celery added

---
+++ backend/app/celery_app.py
@@ -0,0 +1,10 @@
+from celery import Celery
+
+# Celery instance
+celery = Celery(
+    "worker",
+    # redis url
+    broker="redis://localhost:6379/0",
+    # opt result storage
+    backend="redis://localhost:6379/0"
+)
