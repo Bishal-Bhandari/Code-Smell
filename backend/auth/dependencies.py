9be845033a390bad01b9b08faee4bfb15374ecba
bishal <bishalbhaandari62@gmail.com>
2026-02-27 09:46:37 +0100

    verifying the token with OAuth2

--- backend/auth/dependencies.py
+++ backend/auth/dependencies.py
@@ -2,13 +2,32 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from backend.auth.models import get_user_by_email
-from backend.config.config import JWT_SECRET
+from auth.models import get_user_by_email
+from config.config import JWT_SECRET
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/login')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def create_access_token(data: dict, expires_minutes: int = 60):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, JWT_SECRET, algorithm="HS256")
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or expired token",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        email = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = get_user_by_email(email)
+    if user is None:
+        raise credentials_exception
+    return user
