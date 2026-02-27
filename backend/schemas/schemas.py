dbb431a6e44b6faca29bcff7af8f3cd0d642db0f
bishal <bishalbhaandari62@gmail.com>
2026-02-27 09:35:36 +0100

    schemas for login, JWT token, subscription

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -7,3 +7,23 @@ class PRRequest(BaseModel):
     owner: str
     repo: str
     pr_number: int
+
+# Signup/Login
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserLogin(UserBase):
+    password: str
+
+# JWT token
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+# Subscription
+class SubscriptionCheck(BaseModel):
+    subscription: str
+    usage_count: int
