621029d04a61933617857502fa7e99debd73df4a
bishal <bishalbhaandari62@gmail.com>
2026-03-01 18:04:59 +0100

    refactor validate_usage function to update free plan limit and improve error handling

--- backend/auth/subscription.py
+++ backend/auth/subscription.py
@@ -1,12 +1,9 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException
 from auth.models import increment_usage
 
 def validate_usage(user):
-    # free plan max 3 pull request per day
-    if user["subscription"] == "free" and user.get("usage_count", 0) >= 3:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Free plan limit reached. Upgrade to continue."
-        )
-    # Increment usage
+    if user["subscription"] == "free" and user["usage_count"] >= 10:
+        raise HTTPException(status_code=403, 
+                            detail="Free plan limit reached")
+
     increment_usage(user["_id"])
