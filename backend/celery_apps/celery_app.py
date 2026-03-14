540a120fff3c4c4d75de4859b4ed35c1658e047c
bishal <bishalbhaandari62@gmail.com>
2026-03-14 19:35:13 +0100

    feat: add monthly usage reset schedule to Celery beat configuration

--- backend/celery_apps/celery_app.py
+++ backend/celery_apps/celery_app.py
@@ -1,5 +1,6 @@
 # backend/celery_app/celery_app.py
 from celery import Celery
+from celery.schedules import crontab
 
 # Create Celery instance
 celery = Celery(
@@ -10,3 +11,11 @@ celery = Celery(
 
 # Autodiscover tasks inside analysis_engine folder
 celery.autodiscover_tasks(["backend.analysis_engine.tasks"])
+
+#resets montly usages count on 1st of month
+celery.conf.beat_schedule = {
+    "reset-usage-monthly": {
+        "task": "analysis_engine.tasks.reset_monthly_usage",
+        "schedule": crontab(day_of_month=1, hour=0, minute=0),
+    }
+}
